6d709f935a94916288d2f0875f3db8857f60a36a
Tomek Kruszyna <tomek.kruszyna@gmail.com>
2020-04-21 17:23:54 +0200

    Scanner moved to createEmployee

--- src/EmployeeFactory.java
+++ src/EmployeeFactory.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 class EmployeeFactory {
-    private Scanner sc = new Scanner(System.in);
+
 
     public Employee createEmployee() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Podaj imię: ");
         String firstName = sc.nextLine();
         System.out.print("Podaj nazwisko: ");
